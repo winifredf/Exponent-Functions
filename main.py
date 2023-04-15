70a798e4f53cbaa54688e6c068197a80a149d626
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-15 15:34:03 -0400

    print the answer and corrected spacing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def raise_to_power(base_num, pow_num):
-   result = 1
+    result = 1
     for index in range(pow_num):
         result = result * base_num
 
-return result 
+    return result
+
+print(raise_to_power(3, 4))
